4fd5da2d6a3c4e9ec62fd4f86c0d43ad2d14a690
Rming <rmingwang@gmail.com>
2020-06-23 11:28:07 +0800

    update pylintrc

---
+++ 20_ProgressBar.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QProgressBar
+from PyQt5.QtCore import QBasicTimer
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.pbar = QProgressBar(self)
+        self.pbar.setGeometry(30, 40, 200, 25)
+
+        # position
+        self.setGeometry(300, 300, 350, 150)
+        # window title
+        self.setWindowTitle("Slider")
+        # show window
+        self.show()
+
+    def changeValue(self, value):
+        if value == 0:
+            self.label.setPixmap(QPixmap("images/mute.png"))
+        elif 0 < value <= 30:
+            self.label.setPixmap(QPixmap("images/min.png"))
+        elif 30 < value <= 80:
+            self.label.setPixmap(QPixmap("images/med.png"))
+        else:
+            self.label.setPixmap(QPixmap("images/max.png"))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
